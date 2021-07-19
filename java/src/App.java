3e599cb86e3f605f514a398bf5cf409485411e49
peter22126866 <66263594+peter22126866@users.noreply.github.com>
2021-07-20 00:03:23 +0800

    first var

---
+++ java/src/App.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.JToggleButton;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Frame mainFrame = new Frame("眼藥水紀錄器");
+        Panel panel = new Panel();
+        JToggleButton button1 = new JToggleButton("紅藥水");
+        button1.setBackground(Color.PINK);
+        JToggleButton button2 = new JToggleButton("白藥水");
+        button2.setBackground(Color.WHITE);
+
+        mainFrame.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        button1.addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent e){
+                button2.setSelected(false);
+                button1.setEnabled(false);
+                button2.setEnabled(true);
+            }
+        });
+        button2.addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent e){
+                button1.setSelected(false);
+                button2.setEnabled(false);
+                button1.setEnabled(true);
+            }
+        });
+
+        panel.add(button1);
+        panel.add(button2);
+        mainFrame.add(panel);
+        mainFrame.setLocation(525, 350);
+        mainFrame.setSize(350,150);
+        mainFrame.setVisible(true);
+    }
+}
